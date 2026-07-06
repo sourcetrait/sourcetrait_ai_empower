b40ac87a3c018c745b7727d9e6dac35650ba58d5
roylaurie <roy.laurie.pub@sourcetrait.com>
2026-07-06 10:47:44 -0700

    equipment rig model working

--- crates/sourcetrait_equipment/src/model/toml/library.rs
+++ crates/sourcetrait_equipment/src/model/toml/library.rs
@@ -5,34 +5,10 @@ pub struct LibraryToml {
     pub name: String,
     pub title: String,
     pub version: String,
-    pub provider: LibraryTomlProvider,
-    pub description: LibraryTomlDescription,
-    pub license: LibraryTomlLicense,
+    pub provider: TomlProvider,
+    pub description: TomlDescription,
+    pub license: TomlLicense,
     pub dependencies: HashMap<String, LibraryTomlDependency>,
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-pub struct LibraryTomlAuthor {
-    pub name: String,
-    pub title: String,
-    pub email: String,
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-pub struct LibraryTomlProvider {
-    pub name: String,
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-pub struct LibraryTomlDescription {
-    pub summary: String,
-    pub keywords: Vec<String>,
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-pub struct LibraryTomlLicense {
-    pub spdx: Option<String>,
-    pub file: PathBuf,
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
--- crates/sourcetrait_equipment/src/model/toml/rig.rs
+++ crates/sourcetrait_equipment/src/model/toml/rig.rs
@@ -5,38 +5,39 @@ pub struct RigToml {
     pub name: String,
     pub title: String,
     pub version: String,
-    pub provider: RigTomlProvider,
-    pub description: RigTomlDescription,
-    pub license: RigTomlLicense,
-    pub libraries: Vec<String>,
+    pub author: TomlAuthor,
+    pub provider: TomlProvider,
+    pub description: TomlDescription,
+    pub license: TomlLicense,
+    pub exports: RigTomlExports,
     pub nushell: RigTomlNushell,
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
-pub struct RigTomlAuthor {
-    pub name: String,
-    pub title: String,
-    pub email: String,
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-pub struct RigTomlDescription {
-    pub summary: String,
-    pub keywords: Vec<String>,
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-pub struct RigTomlLicense {
-    pub spdx: Option<String>,
-    pub file: PathBuf,
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-pub struct RigTomlProvider {
-    pub name: String,
+pub struct RigTomlExports {
+    pub libraries: Vec<String>,
 }
 
 #[derive(Debug, serde::Serialize, serde::Deserialize)]
 pub struct RigTomlNushell {
     pub version: String,
+}
+
+impl RigToml {
+    pub const RIG_TOML: &'static str = "rig.toml";
+    
+    pub fn read<P: AsRef<Path> + Into<PathBuf>>(path: P) -> EquipmentResult<Self> {
+        let path = if path.as_ref().is_dir() {
+            path.as_ref().join(Self::RIG_TOML)
+        } else {
+            path.into()
+        };
+        
+        let txt = fs::read_to_string(&path)
+            .map_err(|source| EquipmentError::File { source, path: path.to_path_buf(), op: IoErr::Read })?;
+        let rig = toml::from_str(&txt)
+            .map_err(|source| EquipmentError::TomlFileRead { source, path: path.to_path_buf() })?;
+        
+        Ok(rig)
+    }
 }
